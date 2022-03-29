030dc6077eda324e2c1616b7c426674e330ac37e
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-29 07:43:33 -0400

    Feat: create update action in item controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,19 +1,29 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :update]
+
   def index
     render json: Item.all
   end
 
   def show
-    render json: Item.find(params[:id])
+    render json: @item
   end
 
   def create
     render json: Item.create(item_params)
   end
 
+  def update
+    render json: @item.update(item_params)
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
