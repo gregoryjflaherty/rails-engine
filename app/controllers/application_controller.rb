f27e73e771ef0dbc8fbd75a4a854bc29b6a9592c
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-30 13:18:36 -0400

    Feat: add exception handling to action controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_message
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_message
+
+   def not_found_message(exception)
+     render json: { status: "Not Found"}, status: 404
+   end
+
+   def invalid_message(exception)
+     render json: exception.record.errors, status: :not_found
+   end
 end
