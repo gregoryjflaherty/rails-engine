6949262709236bef2b4d49aac4bc578289123285
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-30 13:19:53 -0400

    Refactor: edit item show spec

--- spec/requests/api/v1/items/show_request_spec.rb
+++ spec/requests/api/v1/items/show_request_spec.rb
@@ -10,19 +10,19 @@ describe "Item Show API" do
 
     expect(response).to be_successful
 
-    expect(item).to have_key(:id)
-    expect(item[:id]).to be_an(Integer)
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to be_an(String)
 
-    expect(item).to have_key(:name)
-    expect(item[:name]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
 
-    expect(item).to have_key(:description)
-    expect(item[:description]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
 
-    expect(item).to have_key(:unit_price)
-    expect(item[:unit_price]).to be_a(Float)
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
 
-    expect(item).to have_key(:merchant_id)
-    expect(item[:merchant_id]).to be_a(Integer)
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
   end
 end
