5744ca9f10534b3f1cab1c6bcb51aebf3275fda7
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-01 08:41:37 -0400

    Feat: add merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.search_name(name)
+    where('name ILIKE ?', "%#{name}%").first
+  end
+
+  def self.search_all_names(name)
+    where('name ILIKE ?', "%#{name}%").limit(18).order(:name)
+  end
 end
